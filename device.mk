41dffda9eeaa55824efec149f5b9e5061ac7e6a5
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-12-25 17:48:38 -0500

    hammerhead: Add dual booting support

--- device.mk
+++ device.mk
@@ -20,6 +20,9 @@
 # Everything in this directory will become public
 
 PRODUCT_COPY_FILES += \
+    device/lge/hammerhead/init.multiboot.mounting.sh:root/init.multiboot.mounting.sh \
+
+PRODUCT_COPY_FILES += \
     device/lge/hammerhead/init.hammerhead.rc:root/init.hammerhead.rc \
     device/lge/hammerhead/init.hammerhead.usb.rc:root/init.hammerhead.usb.rc \
     device/lge/hammerhead/fstab.hammerhead:root/fstab.hammerhead \
